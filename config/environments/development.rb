d62f8e9a612b5bc1aeee59ed804672a85b3127cc
devaniljr <contatodevanil@gmail.com>
2023-01-19 00:35:01 -0400

    feat: real-time search with hotwire

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # To make Tailwind work:
+  config.assets.debug = true
+
   # Do not eager load code on boot.
   config.eager_load = false
 
